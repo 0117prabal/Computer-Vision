e16fea7d60a129193277d287b28e2aa226a21aa0
Muhammad Omer Saeed <mu.saeed@jacobs-university.de>
2015-01-06 00:52:21 +0100

    added new files

---
+++ Hough transform, kmeans, mean shift/src/HoughTransform.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+class HoughTransform{
+
+private:
+
+	Mat image;
+
+public:
+
+	HoughTransform(string path){
+
+		image = imread(path, 1);
+
+		if(!image.data){
+
+			cout<<"Unable to read image"<<endl;
+		}
+
+	}
+
+	void run(){
+
+		detectUsingBuiltInFunction();
+		withoutUsingBuiltInFunction();
+	}
+
+	void detectUsingBuiltInFunction(){
+
+		Mat image1;
+		Mat image2 = image.clone();
+		cvtColor(image, image1, COLOR_BGR2GRAY);
+
+		vector<Vec3f> circles;
+		HoughCircles(image1, circles, CV_HOUGH_GRADIENT, 2, 40, 20, 11, 2, 18);
+
+		for(int i = 0 ; i < circles.size() ; i++){
+
+			circle(image2, Point(static_cast<int>( round(circles[i][0])), static_cast<int>( round(circles[i][1]))), static_cast<int>( round(circles[i][2])), Scalar(0, 0, 255), 1, 8, 0);
+		}
+
+//		imshow("Detected circles using HoughCircles", image2);
+//		waitKey(0);
+	}
+
+	void withoutUsingBuiltInFunction(){
+
+		Mat image1;
+		Mat edges;
+
+		cvtColor(image, image1, COLOR_BGR2GRAY);
+		Canny(image1, edges, 300, 400);
+
+		vector<Mat> accum;
+
+		for(int i = 1 ; i < 6 ; i++){
+
+			Mat temp = Mat::zeros(image1.rows/2, image1.cols/2, CV_32S);
+			accum.push_back(temp);
+
+		}
+		
+		edges.convertTo(edges, CV_32F);
+
+		for(int i = 0 ; i < edges.rows ; i++){
+
+			for(int j = 0 ; j < edges.cols ; j++){
+
+				if(edges.at<float>(i,j) == 0.f){
+
+					continue;
+
+				}
+
+
+				for(int r = 0 ; r < 5 ; r++){
+
+					for(double theta = 0.0; theta < M_PI*2 ; theta += 0.1){
+
+						int a = static_cast<int>(round((i+1) - (r+1)*cos(theta)));
+						int b = static_cast<int>(round((j+1) + (r+1)*sin(theta)));
+
+						if (a<0 || b<0 || a>=accum[r].cols || b>=accum[r].rows){
+
+						  	continue; 
+
+						}
+
+						accum[r].at<int>(a,b)++;
+					}
+
+				}
+
+			}
+		}
+	}
+
+	~HoughTransform(){
+
+		//nothing to do here
+	}
+};
---
+++ Hough transform, kmeans, mean shift/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "HoughTransform.cpp"
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char** argv){
+
+	HoughTransform ht("images/circles.png");
+	ht.run();
+
+	return 0;
+}
