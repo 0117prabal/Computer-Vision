482d1dbbf5811620b49b6fcd6b3dd62ce6629060
Muhammad Omer Saeed <mu.saeed@jacobs-university.de>
2015-01-14 01:10:19 +0100

    improved code

--- Snakes, Geodesic active contours/src/Snakes.cpp
+++ Snakes, Geodesic active contours/src/Snakes.cpp
@@ -10,7 +10,7 @@ class Snakes{
 
   private:
 
-      Mat image;
+    Mat image;
 
   public:
 
@@ -67,6 +67,35 @@ class Snakes{
     GaussianBlur(gx, gx, cv::Size(), 5);
     GaussianBlur(gy, gy, cv::Size(), 5);
 
+    int kkkTOTAL = 9;
+
+    // iterate until
+    // - optimal solution for every point is the center of the box, OR
+    // - until maximum number of iterations is reached
+    int iii = 0, nodesChanged;
+
+    do{
+        iii++;
+        nodesChanged = 0;
+
+        const float dist = averageDistance(snake);
+        Mat_ <float> energies = Mat::zeros(kkkTOTAL, verticesNumber, CV_32FC1); // this will hold the costs for each node and each state
+        Mat_ <int>   position = Mat::zeros(kkkTOTAL, verticesNumber, CV_32SC1); // this will hold information about the minimum cost route to reach each node
+
+        for (int vvv=0; vvv < verticesNumber; vvv++){
+          // test all possible states (local neighborhood) of current (ccc) node
+          for (int ccc=0; ccc<kkkTOTAL; ccc++){
+
+            int cccOFF_Y, cccOFF_X;
+            define_OFFs(ccc, cccOFF_Y, cccOFF_X);
+
+
+          }
+        }
+
+
+      } while(iii < 10000 &&& nodesChanged > 0);
+
   }
 
 
@@ -84,7 +113,75 @@ class Snakes{
         circle(img, p, 2, cv::Scalar(1,0,0), -1 );      
     }
 
+  }
+
+
+  float averageDistance(vector<Point2i> & snake){
+    
+    const int siz = snake.size();
+    float dist = 0;
+
+    for(int i = 0; i < snake.size(); i++){
+           
+           dist += sqrt(pow(snake[(i+1)%siz].x - snake[i].x, 2) + pow(snake[(i+1)%siz].y - snake[i].y, 2));
+
+      }
+
+    return dist / (float)siz;
+
+  }
+
+  void define_OFFs( const int& kkk, int& off_Y, int& off_X ){
+    
+    switch (kkk){ 
+           //             i            j
+        case 0:
+            off_Y =  0;
+            off_X =  0;
+            break; ///
+        
+        case 1:
+            off_Y =  0;
+            off_X = -1;
+            break;
+        
+        case 2:
+            off_Y =  0;  
+            off_X = +1;  
+            break;
+
+        case 3:
+            off_Y = -1;
+            off_X =  0;  
+            break;
+
+        case 4: 
+            off_Y = -1;  
+            off_X = -1;  
+            break;
+
+        case 5:
+            off_Y = -1;  
+            off_X = +1;  
+            break;
+
+        case 6: 
+            off_Y = +1;  
+            off_X =  0;  
+            break;
+
+        case 7:
+            off_Y = +1;  
+            off_X = -1;  
+            break;
+
+        case 8:
+            off_Y = +1;  
+            off_X = +1;  
+            break;
     }
+
+  }
 
   ~Snakes(){
 
